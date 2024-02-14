2a1807918d1d1cee67412ea72f4e7c2e65e774c4
Kolbi03 <lengyel.milan.hu@gmail.com>
2024-02-14 14:32:48 +0100

    Login screen progress

--- App.tsx
+++ App.tsx
@@ -1,11 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, Text, View, TextInput, Button, FlatList} from 'react-native';
-import React, {useState} from "react";
 import LoginScreen from "./login/login";
 
 export default function App() {
-  /*const [isLoggedIn, setLoggedIn] = useState(false);
-  const [text, setText] = useState('');*/
   return (
     LoginScreen()
 
--- login/login.tsx
+++ login/login.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, Text, View, TextInput, Button, FlatList} from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
 import React, {useState} from "react";
 
 export default function LoginScreen() {
