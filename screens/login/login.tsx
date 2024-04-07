b6e6d14b8f5c3f1ed934fec3900acf926b104ec0
Lengyel Milán <lengyel.milan.hu@gmail.com>
2024-04-07 12:43:02 +0200

    Added reanimated plugin, design overhaul started

--- screens/login/login.tsx
+++ screens/login/login.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, TextInput } from 'react-native';
+import {Text, View, TextInput, Image} from 'react-native';
 import React, {useContext, useState} from "react";
 import { Pressable } from "react-native";
 import Styles from "../../Stylesheet";
 import loginDataDTO from '../../interfaces/loginDataDTO';
 import {AuthContext} from "../../contexts/authContext";
+import Animated, {FadeIn, FadeInUp, useSharedValue} from "react-native-reanimated";
 
 // @ts-ignore
 const LoginScreen = ({ navigation }) => {
@@ -22,37 +23,46 @@ const LoginScreen = ({ navigation }) => {
     }
 
     return (
-        <View style={styles.container}>
+        <Animated.View className="bg-white h-full- w-full flex-1">
             <StatusBar style='auto' />
+            <Image className="h-full w-full absolute"/>
 
-            <Text style={styles.header}>Login</Text>
+            <View className="h-full w-full flex justify-center">
+                <Animated.View className="items-center" entering={FadeInUp.delay(100).duration(600)}>
+                    <Text style={styles.header}>Login</Text>
+                </Animated.View>
 
-            {/*Email mező*/}
-            <Text style={styles.text}>Email</Text>
+                {/*Email mező*/}
+                <View className="flex items-center mx-4 space-y-4">
+                    <Animated.View className="w-full bg-black/5 rounded-2xl p-5 h-14" entering={FadeInUp.delay(200).duration(600)}>
+                        <TextInput placeholder='Email' keyboardType="email-address" onChangeText={email => setEmail(email)} placeholderTextColor={'gray'}/>
+                    </Animated.View>
 
-            <View style={styles.textInput}>
-                <TextInput placeholder='Bob@monke.com' keyboardType="email-address" onChangeText={email => setEmail(email)} placeholderTextColor={'gray'}/>
+                    {/*Jelszó mező*/}
+                    <Animated.View className="w-full" entering={FadeInUp.delay(300).duration(600)} >
+                        <Animated.View className="w-full bg-black/5 rounded-2xl p-5 h-14" entering={FadeInUp.delay(200).duration(600)}>
+                            <TextInput placeholder='Password' secureTextEntry={true} onChangeText={password => setPassword(password)} placeholderTextColor={'gray'}/>
+                        </Animated.View>
+                    </Animated.View>
+
+                    <Animated.View className="w-full" entering={FadeInUp.delay(400).duration(600)}>
+                        <Pressable onPress={() => {
+                            login({email: data.email, password: data.password})
+                        }}>
+                            <Text style={styles.pressButton}>Login</Text>
+                        </Pressable>
+                    </Animated.View>
+
+                    <Animated.View className="w-full" entering={FadeInUp.delay(500).duration(600)}>
+                        <Pressable onPress={() => {
+                            navigation.navigate('Register')
+                        }}>
+                            <Text style={styles.pressButton}>Register</Text>
+                        </Pressable>
+                    </Animated.View>
+                </View>
             </View>
-
-            {/*Jelszó mező*/}
-            <Text style={styles.text}>Password</Text>
-
-            <View style={styles.textInput}>
-                <TextInput placeholder='********' secureTextEntry={true} onChangeText={password => setPassword(password)} placeholderTextColor={'gray'}/>
-            </View>
-
-            <Pressable onPress={() => {
-                login({email: data.email, password: data.password})
-            }}>
-                <Text style={styles.pressButton}>Login</Text>
-            </Pressable>
-
-            <Pressable onPress={() => {
-                navigation.navigate('Register')
-            }}>
-                <Text style={styles.pressButton}>Register</Text>
-            </Pressable>
-        </View>
+        </Animated.View>
     );
 }
 
